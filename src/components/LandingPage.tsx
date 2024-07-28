a9514ecbd23ed2d2b2b7bf0095ee6eb9b1d62df3
Abukarabukar <159937083+Abukarabukar@users.noreply.github.com>
2024-07-28 19:41:58 -0400

    I added the landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/LandingPage.scss';
+import myPhoto from '../assets/myPhoto.jpg';
+import { FlipWords } from './FlipWords';
 
 const LandingPage: React.FC = () => {
+  const quoteWords = [
+    "chase",
+    "reach",
+    "fly",
+    
+  ];
+
   return (
     <div className="landing-page">
-      <h1>Welcome to Abukar's website</h1>
-      <p>Are you bored?</p>
-      <Link to="/tictactoe">
-        <button className="start-game-btn">Start the Tic Tac Toe Game</button>
-      </Link>
+      <div className="photo-container">
+        <img src={myPhoto} alt="Abukar" className="profile-photo" />
+      </div>
+      <div className="quote">
+      <span className="static-text">I will</span>&nbsp;
+        <FlipWords words={quoteWords} duration={2000} className="text-2xl font-bold" />
+      </div>
+      <p className="question">Are you bored now?</p>
+      <Link to="/game" className="start-game-btn">Click here to play!</Link>
     </div>
   );
 };
