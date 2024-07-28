7664797ec633fbb1321e4d97021a28322aa23f56
Abukarabukar <159937083+Abukarabukar@users.noreply.github.com>
2024-07-28 19:51:18 -0400

    I added the landing page with video background

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,26 +3,32 @@ import { Link } from 'react-router-dom';
 import '../styles/LandingPage.scss';
 import myPhoto from '../assets/myPhoto.jpg';
 import { FlipWords } from './FlipWords';
+import backgroundVideo from '../assets/4k.mp4';
 
 const LandingPage: React.FC = () => {
   const quoteWords = [
     "chase",
     "reach",
     "fly",
-    
   ];
 
   return (
     <div className="landing-page">
-      <div className="photo-container">
-        <img src={myPhoto} alt="Abukar" className="profile-photo" />
+      <video autoPlay loop muted className="background-video">
+        <source src={backgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <div className="photo-container">
+          <img src={myPhoto} alt="Abukar" className="profile-photo" />
+        </div>
+        <div className="quote">
+          <span className="static-text">I will</span>&nbsp;
+          <FlipWords words={quoteWords} duration={2000} className="text-2xl font-bold" />
+        </div>
+        <p className="question">Are you bored now?</p>
+        <Link to="/game" className="start-game-btn">Click here to play!</Link>
       </div>
-      <div className="quote">
-      <span className="static-text">I will</span>&nbsp;
-        <FlipWords words={quoteWords} duration={2000} className="text-2xl font-bold" />
-      </div>
-      <p className="question">Are you bored now?</p>
-      <Link to="/game" className="start-game-btn">Click here to play!</Link>
     </div>
   );
 };
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,6 +3,10 @@ declare module '*.mp3' {
     export default src;
   }
   
+  declare module '*.mp4' {
+    const src: string;
+    export default src;
+  }
   declare module '*.gif' {
     const src: string;
     export default src;
